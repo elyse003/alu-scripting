7f19b8e50b098b1d8bdb95f9bb8f7cccfcfd7760
elyse003 <e.uyiringiy@alustudent.com>
2024-07-03 19:15:41 +0000

    made some changes

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,10 +3,8 @@
     This module contains the function number_of_subscribers
 '''
 
-
 import requests
 from sys import argv
-
 
 def number_of_subscribers(subreddit):
     '''
@@ -15,18 +13,15 @@ def number_of_subscribers(subreddit):
     user_agent = {'User-Agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     try:
-        response = requests.get(url, headers=user_agent)
-        response.raise_for_status()  # Raise an error for bad status codes
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data:", e)
+        response = requests.get(url, headers=user_agent, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    except requests.exceptions.RequestException:
         return 0
-    except KeyError:
-        print("Subreddit not found or unable to fetch data.")
-        return 0
-
 
 if __name__ == "__main__":
     if len(argv) != 2:
@@ -35,3 +30,4 @@ if __name__ == "__main__":
     subreddit_name = argv[1]
     subscriber_count = number_of_subscribers(subreddit_name)
     print(subscriber_count)
+
