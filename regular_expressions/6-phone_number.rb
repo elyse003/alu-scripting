a61e91d74809daefbd422776489e86cc6d4237fc
elyse003 <e.uyiringiy@alustudent.com>
2024-07-05 20:03:04 +0000

    0-subs.py

--- regular_expressions/6-phone_number.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-#Script to match 10 digits
-puts ARGV[0].scan(/^\d{10}$/).join
--- regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-#Script to match capital letters
-puts ARGV[0].scan(/[A-Z]/).join
