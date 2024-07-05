962db4c312ef3bd31061fd9f38da2c8e5e48bf27
elyse003 <e.uyiringiy@alustudent.com>
2024-07-05 20:03:04 +0000

    0-subs.py

--- regular_expressions/0-simply_match_school.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-#Script to match School
-puts ARGV[0].scan(/School/).join
--- regular_expressions/8-textme.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-#Script to output sender and receiver info and flags
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
